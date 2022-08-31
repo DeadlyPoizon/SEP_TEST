aa29096ec61cedc5b124d1a460e450969fddac7c
poizo <wgj>
2022-08-31 10:47:09 +0200

    Initial commit

---
+++ API Test/Program.cs
@@ -0,0 +1,65 @@
+﻿/*using System.Net;
+using Microsoft.Data.Analysis;
+namespace ConsoleTests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string QUERY_URL =
+                "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=BHS2KBKO7OUQXJFK";
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(QUERY_URL);
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+            StreamReader sr = new StreamReader(resp.GetResponseStream());
+            string results = sr.ReadToEnd();
+            sr.Close();
+            File.WriteAllText("data.csv", results);
+
+            DataFrame df = DataFrame.LoadCsv("data.csv");
+            Console.WriteLine(df);
+            
+        }
+    }
+}*/
+
+using System;
+using System.IO;
+using System.Net;
+using Microsoft.Data.Analysis;
+
+namespace StockProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AVConnection conn = new AVConnection("BHS2KBKO7OUQXJFK");
+            conn.SaveCSVFromURL("IBM");
+            DataFrame df = DataFrame.LoadCsv("stockdata.csv");
+            Console.WriteLine(df);
+        }
+    }
+
+    public class AVConnection
+    {
+        private readonly string _apiKey;
+
+        public AVConnection(string apiKey)
+        {
+            this._apiKey = apiKey;
+        }
+
+        public void SaveCSVFromURL(string symbol)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://" + $@"www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={this._apiKey}&datatype=csv");
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+            StreamReader sr = new StreamReader(resp.GetResponseStream());
+            string results = sr.ReadToEnd();
+            sr.Close();
+            File.WriteAllText("stockdata.csv", results);
+        }
+    }
+}
